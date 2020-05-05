907e51e86d5c1f569d0c41a7af6fd503e3481081
katherine Lundy <katherinersl@gmail.com>
2020-05-05 00:01:15 -0400

    psuedo coded basic format of game of what I want to do in Javascript and HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,12 @@
-//
+//Advance Trivia game!
+
+//Game has 8 questions and each question has 4 multiple choice questions/answers.
+
+//Once questions is answered by the selector a video image pops up announcing win or lose. The image is part of the answer
+
+//create a function for a random pick for questions being displayed for multiple choice. answers are displayed and you have a 30 sec timer once "start"button clicked.
+
+// will craete a container using bootstrap to place "p" and multiple choice in it. 
+
+// after each question is completed by user, the page loads to next multiple choice question with the 30 sec timer starting over.
+
